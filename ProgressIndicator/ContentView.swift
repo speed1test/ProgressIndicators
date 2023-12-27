87559a383976590ec0c4afbf7505f9926fda6ee5
Edwin <edwin12sv@gmail.com>
2023-12-26 22:08:27 -0600

    Initial Commit

---
+++ ProgressIndicator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProgressIndicator
+//
+//  Created by elsalvador on 26/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProgressIndicator/ProgressIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressIndicatorApp.swift
+//  ProgressIndicator
+//
+//  Created by elsalvador on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ProgressIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
